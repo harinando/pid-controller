28c007323c390f4160a16f38e7aa3cfe0d3845e5
Harinando Andrianarimanana <toni.andrianaht@gmail.com>
2017-06-18 13:01:22 -0700

    adding simple readme to instal-ide-profile

--- install-ide-profile.sh
+++ install-ide-profile.sh
@@ -1,2 +1,5 @@
-mkidr -p ide_profiles/xcode & cd ide_profiles/xcode
+mkdir -p ide_profiles/xcode && cd ide_profiles/xcode
 cmake -G "Xcode" ../..
+# Adding README
+echo "# behavioral-cloning" >> README.md
+echo "https://github.com/udacity/CarND-Extended-Kalman-Filter-Project/tree/master/ide_profiles/xcode" >> README.md
